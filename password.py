6e5f21d927e5daa37d73af3a2606adbd69ec39fa
Jimmy <mail@jmath.read-books.org>
2016-04-07 17:41:14 -0400

    Most rules unimplemented

---
+++ password.py
@@ -0,0 +1,137 @@
+#!/usr/bin/python
+
+import subprocess
+import thread
+import time
+class clipper:
+	def __init__(self):
+		self.clipboard_function = "none"
+		try:
+			subprocess.check_output(["xclip", "-h"])
+			self.clipboard_function = "xclip"
+		except:
+			pass
+		try:
+			subprocess.check_output(["clipit", "-h"])
+			self.clipboard_function = "clipit"
+		except:
+			pass
+
+	def clip(self,str):
+		if self.clipboard_function == "clipit":
+			subprocess.check_call(["clipit", str])
+			return
+		if self.clipboard_function == "xsel":
+			subprocess.check_call(["echo", str, "| xsel"])
+			return
+		if self.clipboard_function == "xclip":
+			subprocess.check_call(["echo", str, "| xclip"])
+			return
+
+	def clear(self):
+		self.clip("cleared")
+
+	def delayed_clear(self, amount):
+		time.sleep(amount)
+		self.clear()
+
+import csv
+class password_settings:
+	def __init__(self, filename):
+		self.account_names = []
+		self.accounts = []
+		self.default_rules = []
+		try:
+			reader = csv.reader(open(filename))
+			reader.next()
+			for line in reader:
+				if len(line)>0:
+					rule_list =	{"account"		: line[0].strip(),
+							"character limit"	: int0(line[1].strip()),
+							"character minimum"	: int0(line[2].strip()),
+							"letter minimum"	: int0(line[3].strip()),
+							"capital minimum"	: int0(line[4].strip()),
+							"symbol minimum"	: int0(line[5].strip()),
+							"expiration"		: line[6].strip(),
+							"manual appendage"	: line[7].strip()}
+				self.accounts.append(rule_list)
+			for rule_list in self.accounts:
+				self.account_names.append(rule_list["account"])
+			self.default_rules = self.accounts[0]
+		except:
+			self.default_rules = 	{"account":"default",
+						"character limit":15,
+						"character minimum"     : 6,
+						"letter minimum"        : 0,
+						"capital minimum"       : 0,
+						"symbol minimum"        : 0,
+						"expiration"            : "",
+						"manual appendage"      : ""}
+
+	def int0(input):
+		try:
+			return int(input)
+		except:
+			return 0
+
+	def get(self, n):
+		return self.accounts[self.account_names.index(n)]
+
+import hashlib
+class hasher:
+	def __init__(self, input):
+		self.buffer = input
+
+	def set_rules(self, r):
+		self.rules = r
+
+	def apply_input_mask(self):
+		copy = self.buffer
+		#...act on copy, using rules
+		self.buffer = copy
+
+	def apply_output_mask(self):
+		character_limit = self.rules["character limit"]
+		if character_limit > 0:
+			copy = self.buffer[:character_limit]
+		self.buffer = copy
+
+	def hash(self):
+		self.buffer = hashlib.sha512(self.buffer).hexdigest()
+
+	def output(self):
+		return self.buffer
+
+
+import getpass
+import os
+
+os.system('clear')
+
+c = clipper()
+settings = password_settings("accounts.csv")
+
+while True:
+	name = raw_input()
+	if name in ["q","quit","exit"]:
+		c.clear()
+		break
+	h = hasher(getpass.getpass("")+name)
+	print '\033[F\033[F'
+
+	if name in settings.account_names:
+		h.set_rules(settings.get(name))
+	else:
+		h.set_rules(settings.default_rules)
+
+	h.apply_input_mask()
+	h.hash()
+	h.apply_output_mask()
+
+	if c.clipboard_function != "none":
+		c.clip(h.output())
+		thread.start_new_thread(c.delayed_clear, (5,) )
+	else:
+		print h.output()
+
+
---
+++ readme.md
@@ -0,0 +1,63 @@
+###Password Hash
+
+This script is a simple, unusual password manager. You only need one master password, and it is never stored for more than a second.
+
+1. Run the Python script at the terminal.
+2. Enter a string describing the account that the password is for, like *gmail* or *bankofamerica*.
+3. Enter your master password.
+4. The two are hashed, and if desired, character limits are imposed and symbols added or subtracted to meet the account provider's requirements.
+5. The result is copied to the system clipboard, if possible, then erased in 5 seconds. Otherwise it is printed to the terminal.
+
+###Setting password rules for an account
+
+List the password limitations in the file "accounts.csv", in the format:
+
+    name, character max, character min, letter min, capital min, symbol min, expiration, manual appendage
+    default,         15,              ,           ,            ,           ,           ,
+    gmail,           15,            10,          8,           2,          0,           ,
+    bankofamerica,   15,            10,          8,           2,          0,           ,
+    tfcu,            15,            10,          8,           2,          0,           ,
+    netid,         1000,            10,          8,           2,          0,           ,
+
+* *name*  
+The name of the website or provider, e.g. *gmail*.
+
+* *character max* (number or blank)  
+Limits the number of characters in the final output.		
+
+* *character min* (number or blank)  
+Ensures a minimum number of characters in the final output.
+
+* *letter min* (number or blank)  
+Ensures a minimum number of letter characters in the final output.
+
+* *capital min* (number or blank)  
+Ensures a minimum number of capital letter characters in the final output.
+
+* *symbol min* (number or blank)  
+Ensures a minimum number of symbol characters (those arising as shift modifications of a numeral) in the final output.
+
+* *expiration* (weekly, monthly, or yearly, or number of days)  
+Appends a string identifying the week/month/year to the hash.
+
+* *manual appendage* (string)  
+Appends the string to the hash. For manual "versions". 
+
+###How it looks
+
+	$ python password.py
+
+The screen clears.
+
+	$ gmail
+	$ 
+
+Your typed password doesn't appear on the screen.  
+If *xclip*, *xsel*, or *clipit* are not found, the hash prints:
+
+	$ gmail
+	$ d644d4c2d9e3c49
+
+To exit, use *q*, *quit*, or *exit*:
+
+	$ q
