cf0ad6fa0e88bba6a0e956636cab9ec8c5ff9241
jimmy <imasleepin@gmail.com>
2016-04-09 20:33:35 -0400

    Most masks implemented.

--- password.py
+++ password.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python
 
+class debug_logger:
+
+	def __init__(self):
+		self.message = "Start of log.\n"
+
+	def log(self,new_message):
+		if type(new_message) is not list:
+			self.message = self.message + new_message + "\n"
+		else:
+			for m in new_message:
+				self.message = self.message + "  " + m + "\n"
+
+	def dump(self):
+		print self.message
+
+d = debug_logger()
+
 import os
 import subprocess
 import thread
@@ -49,28 +66,30 @@ class account_settings:
 			for line in reader:
 				if len(line)>0:
 					rule_list =	{"account"		: line[0].strip(),
-							"character limit"	: int0(line[1].strip()),
-							"character minimum"	: int0(line[2].strip()),
-							"letter minimum"	: int0(line[3].strip()),
-							"capital minimum"	: int0(line[4].strip()),
-							"symbol minimum"	: int0(line[5].strip()),
+							"character limit"	: self.int0(line[1].strip()),
+							"letter minimum"	: self.int0(line[2].strip()),
+							"numeral minimum"	: self.int0(line[3].strip()),
+							"capital minimum"	: self.int0(line[4].strip()),
+							"symbol minimum"	: self.int0(line[5].strip()),
 							"expiration"		: line[6].strip(),
 							"manual appendage"	: line[7].strip()}
 				self.accounts.append(rule_list)
 			for rule_list in self.accounts:
 				self.account_names.append(rule_list["account"])
+			d.log("Account names:")
+			d.log(self.account_names)
 			self.default_rules = self.accounts[0]
 		except:
 			self.default_rules = 	{"account":"default",
-						"character limit":15,
-						"character minimum"     : 6,
+						"character limit"       : 15,
 						"letter minimum"        : 0,
+						"numeral minimum"       : 0,
 						"capital minimum"       : 0,
 						"symbol minimum"        : 0,
 						"expiration"            : "",
 						"manual appendage"      : ""}
 
-	def int0(input):
+	def int0(self,input):
 		try:
 			return int(input)
 		except:
@@ -80,6 +99,7 @@ class account_settings:
 		return self.accounts[self.account_names.index(n)]
 
 
+import time
 import hashlib
 class hasher:
 
@@ -88,16 +108,93 @@ class hasher:
 
 	def set_rules(self, r):
 		self.rules = r
+		d.log("Using rules for " + r["account"] + ".")
 
 	def apply_input_mask(self):
 		copy = self.buffer
-		#...act on copy, using rules
+		r = self.rules
+		d.log("Applying input mask.")
+
+		if(r["expiration"] in ["daily", "day"]):
+			copy = copy + time.strftime("%d/%m/%Y")
+			d.log("Daily expiration.")
+		if(r["expiration"] in ["monthly", "month"]):
+			copy = copy + time.strftime("%m/%Y")
+			if time.strftime("%d") == "01":
+				print(r["account"]+" password changed today.")
+			d.log("Monthly expiration.")
+		if(r["expiration"] in ["yearly", "year"]):
+			copy = copy + time.strftime("%Y")
+			if time.strftime("%d%m") == "0101":
+				print(r["account"]+" password changed today.")
+			d.log("Yearly expiration.")
+
+		if r["manual appendage"] != "":
+			d.log("Adding manual appendage for account " + name + ".")
+			copy = copy + r["manual appendage"]
+
 		self.buffer = copy
 
 	def apply_output_mask(self):
-		character_limit = self.rules["character limit"]
-		if character_limit > 0:
-			copy = self.buffer[:character_limit]
+		r = self.rules
+		copy = self.buffer
+
+		if r["character limit"] > 0:
+			copy = copy[:r["character limit"]]
+		else:
+			copy = copy[:15]
+
+		l = self.lower_count(copy)
+		u = self.upper_count(copy)
+		if self.letter_count(copy) != (l + u): 
+			d.log("Alphabetical character count failed.")
+		n = self.numeral_count(copy)
+		s = self.symbol_count(copy)	#always zero?
+
+		#enforce letter min
+		letter_defect = r["letter minimum"] - l - u
+		if letter_defect > 0:
+			list_version = list(copy)
+			switched_count = 0
+			for i in range(len(copy)):
+				if not list_version[i].isalpha() and switched_count<letter_defect:
+					list_version[i] = "a"
+					switched_count = switched_count + 1
+			copy = "".join(list_version)
+
+		#enforce numeral min
+		numeral_defect = r["numeral minimum"] - n
+		if numeral_defect > 0:
+			list_version = list(copy)
+			switched_count = 0
+			for i in range(len(copy)):
+				if list_version[i] not in ["1","2","3","4","5","6","7","8","9","0"] and switched_count<numeral_defect:
+					list_version[i] = "6"
+					switched_count = switched_count + 1
+			copy = "".join(list_version)
+		
+		#enforce capital min
+		capital_defect = r["capital minimum"] - u
+		if capital_defect > 0:
+			list_version = list(copy)
+			switched_count = 0
+			for i in range(len(copy)):
+				if list_version[i].islower() and switched_count < capital_defect:
+					list_version[i] = list_version[i].upper()
+					switched_count = switched_count + 1
+			copy = "".join(list_version)
+
+		#enforce symbol min
+		symbol_defect = r["symbol minimum"] - s
+		if symbol_defect > 0:
+			list_version = list(copy)
+			switched_count = 0
+			for i in range(len(copy)):
+				if list_version[len(copy)-1-i] not in ["!","@","#","$","%","^","&","*","(",")"] and switched_count < symbol_defect:
+					list_version[len(copy)-1-i] = ["!","@","#","$","%","^","&","*","(",")"][(len(copy)-1-i) % 10]
+					switched_count = switched_count + 1
+			copy = "".join(list_version)
+
 		self.buffer = copy
 
 	def hash(self):
@@ -106,25 +203,61 @@ class hasher:
 	def output(self):
 		return self.buffer
 
+	def letter_count(self,input):
+		count=0
+		for char in input:
+			if char.isalpha():
+				count += 1
+		return count
+
+	def lower_count(self,input):
+		count=0
+		for char in input:
+			if char.isalpha() and char.islower():
+				count += 1
+		return count
+
+	def upper_count(self,input):
+		count=0
+		for char in input:
+			if char.isalpha() and char.isupper():
+				count += 1
+		return count
+
+	def numeral_count(self,input):
+		count=0
+		for char in input:
+			if char in ["1","2","3","4","5","6","7","8","9","0"]:
+				count += 1
+		return count
+
+	def symbol_count(self,input):
+		count=0
+		for char in input:
+			if char in ["!","@","#","$","%","^","&","*","(",")"]:
+				count += 1
+		return count
+
 
 import getpass
 
 os.system('clear')
 c = clipper()
-settings = account_settings(".accounts.csv")
+s = account_settings(".accounts.csv")
 
 while True:
 	name = raw_input()
 	if name in ["q","quit","exit"]:
-		c.clear()
 		break
 	h = hasher(getpass.getpass("")+name)
 	print '\033[F\033[F'
 
-	if name in settings.account_names:
-		h.set_rules(settings.get_rules(name))
+	d.log("Account name is " + name + ".")
+
+	if name in s.account_names:
+		h.set_rules(s.get_rules(name))
 	else:
-		h.set_rules(settings.default_rules)
+		h.set_rules(s.default_rules)
 
 	h.apply_input_mask()
 	h.hash()
@@ -136,4 +269,7 @@ while True:
 	else:
 		print h.output()
 
+if c.clipboard_function != "none":
+	c.clear()
 
+d.dump()
--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 
 This script is a simple, unusual password manager. You only need to remember one master password, and it is never stored for more than a second.
 
-1. Run the Python script *password.py* at the terminal.
+1. Run the Python script *password.py* in a terminal emulator (If you'd like, you can change the name).
 2. Enter a string describing the account that the password is for, like *gmail* or *bankofamerica*.
 3. Enter your master password.
 4. The two are hashed, and if desired, character limits are imposed and symbols added or subtracted to meet the account provider's requirements.
@@ -17,36 +17,34 @@ List the password limitations in the file "accounts.csv", in the format:
     gmail,           15,            10,           ,            ,            ,           ,           ,
     bankofamerica,   15,            10,           ,            ,            ,           ,           ,
 
+
 * *name*  
 The name of the website or provider, e.g. *gmail*.
 
 * *character max* (number or blank)  
 Limits the number of characters in the final output.		
 
-* *character min* (number or blank)  
-Ensures a minimum number of characters in the final output.
-
 * *letter min* (number or blank)  
-Ensures a minimum number of letter characters in the final output.
+Tries to ensure a minimum number of letter characters in the final output.
 
 * *numeral min* (number or blank)  
-Ensures a minimum number of numerals in the final output.
+Tries to ensure a minimum number of numerals in the final output.
 
 * *capital min* (number or blank)  
-Ensures a minimum number of capital letter characters in the final output.
+Tries to ensure a minimum number of capital letter characters in the final output.
 
 * *symbol min* (number or blank)  
-Ensures a minimum number of symbol characters (those arising as shift modifications of a numeral) in the final output.
+Tries to ensure a minimum number of symbol characters (those arising as shift modifications of a numeral) in the final output.
 
-* *expiration* (weekly, monthly, or yearly, or number of days)  
-Appends a string identifying the week/month/year to the hash.
+* *expiration* ("weekly", "monthly", "yearly")  
+Appends a string identifying the week/month/year prior to the hash.
 
 * *manual appendage* (string)  
-Appends the string to the hash. For manual "versions". 
+Appends the string prior to the hash. For manual "versions". 
 
 ###How it looks
 
-	$ python password.py
+	$ ./password.py
 
 The screen clears.
 
@@ -67,4 +65,4 @@ To exit, use *q*, *quit*, or *exit*:
 
 * *xclip* is the recommended clipboard manager for this script.
 
-* If using *clipit*, clipboard synchronization is recommended; otherwise program control of the primary clipboard is limited, so that it is difficult to erase the passwords that are copied to the clipboard.
+* If using *clipit*, clipboard synchronization is recommended. Otherwise program control of the primary clipboard is limited; it can be difficult to erase the passwords that are copied to the clipboard.
